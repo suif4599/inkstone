cce4dffd8df0f693b10889d2aa7a66662745141e
suif4599 <suif4599@gmail.com>
2026-09-12 16:30:12 +0800

    chore: local nix env

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Inkstone local development toolchain";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          default = pkgs.mkShell {
+            packages = [ pkgs.nodejs_24 pkgs.git ];
+            npm_config_update_notifier = "false";
+          };
+        });
+    };
+}
